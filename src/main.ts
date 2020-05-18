fc7634f2ad203222049af5bf32d8ff30cee55192
devminchan <minchan.dev@gmail.com>
2020-05-18 17:49:19 +0900

    RedisIoAdapter 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { RedisIoAdapter } from './redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
