69b839502ef1bed6d6243a25615e7dae32f46c44
devminchan <minchan.dev@gmail.com>
2020-05-21 17:15:53 +0900

    Join room 기능 추가

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -3,18 +3,35 @@ import {
   WebSocketGateway,
   MessageBody,
   ConnectedSocket,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { JoinRoomDto } from './chat.dtos';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
 
 @WebSocketGateway()
 export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
   handleMessage(
     @MessageBody() data: string,
-    @ConnectedSocket() client: Socket,
+    @ConnectedSocket() socket: Socket,
   ) {
     console.log(`received message: ${data}`);
-    client.broadcast.emit('message', data);
-    client.emit('message', data);
+    socket.broadcast.emit('message', data);
+    socket.emit('message', data);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @SubscribeMessage('room:join')
+  joinRoom(
+    @MessageBody() joinRoomDto: JoinRoomDto,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(`received message: `, joinRoomDto);
+    socket.join(`r${joinRoomDto.roomId}`);
+    this.server.to(`r${joinRoomDto.roomId}`).emit('user:joined', socket.id);
   }
 }
