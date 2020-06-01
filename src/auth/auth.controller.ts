7eb5ed8407d40d063e399c2fddaaeeb8515ef9c8
devminchan <minchan.dev@gmail.com>
2020-06-01 17:57:36 +0900

    /auth/test endpoint 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtAuthGuard } from './guards/jwt.guard';
-import { LocalAuthRequest, PrincipleRequest } from './auth.interfaces';
+import { LocalAuthRequest } from './auth.interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -12,11 +11,5 @@ export class AuthController {
   @Post('/login')
   async login(@Request() req: LocalAuthRequest) {
     return this.authService.generateToken(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('/test')
-  async test(@Request() req: PrincipleRequest) {
-    return req.user;
   }
 }
