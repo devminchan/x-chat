c3d4aa8a13e9ef8426404340fcdba294adfa1698
devminchan <minchan.dev@gmail.com>
2020-06-19 16:54:49 +0900

    Gateway 내부 예외처리시 WsException 클래스 사용하여 처리

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,16 +4,11 @@ import {
   MessageBody,
   ConnectedSocket,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { JoinRoomDto } from './chat.dtos';
-import {
-  UsePipes,
-  ValidationPipe,
-  UseGuards,
-  BadRequestException,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { WsJwtGuard } from 'src/auth/guards/ws-jwt.guard';
 import { PrincipleSocket } from 'src/auth/auth.interfaces';
 import { ChatService } from './chat.service';
@@ -41,11 +36,11 @@ export class ChatGateway {
 
     // roomId가 없거나, room으로 시작하지 읺는 경우 예외 발생
     if (!currentRoomId) {
-      throw new BadRequestException('socket roomId is missing');
+      throw new WsException('socket roomId is missing');
     }
 
     if (!currentRoomId.startsWith('room')) {
-      throw new BadRequestException('socket roomId is missing');
+      throw new WsException('socket roomId is missing');
     }
 
     let roomId: number;
@@ -54,16 +49,20 @@ export class ChatGateway {
       // roomId를 못 읽어올 시 예외 발생
       roomId = Number.parseInt(currentRoomId.split(' ')[1]);
     } catch (e) {
-      throw new InternalServerErrorException(e);
+      throw new WsException(e);
     }
 
-    const result = await this.chatService.generateChatRecord({
-      userId: socket.user.id,
-      roomId: roomId,
-      message: data,
-    });
+    try {
+      const result = await this.chatService.generateChatRecord({
+        userId: socket.user.id,
+        roomId: roomId,
+        message: data,
+      });
 
-    this.server.to(`room ${roomId}`).emit('message', result);
+      this.server.to(`room ${roomId}`).emit('message', result);
+    } catch (e) {
+      throw new WsException(e.message || e);
+    }
   }
 
   @UsePipes(new ValidationPipe())
@@ -78,8 +77,6 @@ export class ChatGateway {
 
     const user = await this.userService.getUserById(socket.user.id);
 
-    this.server
-      .to(`room ${joinRoomDto.roomId}`)
-      .emit('user:joined', user);
+    this.server.to(`room ${joinRoomDto.roomId}`).emit('user:joined', user);
   }
 }
